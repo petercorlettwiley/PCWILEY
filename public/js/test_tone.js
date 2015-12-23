c521a018c5575781934ae614fc6af012822070dc
petercorlettwiley <peter.c.wiley@gmail.com>
2015-12-23 14:25:51 -0600

    Audio buffering (re: sample sounds and impulse responses for convolvers) is now modular!

--- public/js/test_tone.js
+++ public/js/test_tone.js
@@ -1,36 +1,38 @@
 $( document ).ready(function() {
 
-  var context = new AudioContext();
-  var convolver = context.createConvolver();
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext()
+
+  //var bongo_url = '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409276tin.wav';
+  //var hall_reverb_url = '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409273irHall.ogg';
+
 
   function loadAudio( object, url ) {
-  
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.responseType = 'arraybuffer';
   
     request.onload = function() {
-      context.decodeAudioData(request.response, function(buffer) {
-        object.buffer = buffer;
-        console.log('* buffer * '+buffer);
-      });
+      context.decodeAudioData(
+        request.response,
+        function(buffer) {
+          object.buffer = buffer;
+        },
+        function(e){"Error with decoding audio data" + e.err}
+      );
     }
     request.send();
-
-    console.log( '** LOAD AUDIO ** object: ' + object + ' // url: ' + url );
-
   }
 
-  //var bongo_url = '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409276tin.wav';
-  //var hall_reverb_url = '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409273irHall.ogg';
 
   function SampleSound( sample_url ) {
     var sample_url = sample_url;
   
     loadAudio( this, sample_url );
 
-    this.reverb = true;
     this.volume = context.createGain();
+    //this.volume.connect(c);
+    this.volume.connect(context.destination);
 
     this.play = function () {
       var s = context.createBufferSource();
@@ -38,38 +40,21 @@ $( document ).ready(function() {
       s.connect(this.volume);
       s.start(0);
       this.s = s;
-
-      if (this.reverb === true) {
-        this.convolver = context.createConvolver();
-        this.convolver.buffer = irHall.buffer;
-        this.volume.connect(this.convolver);
-        this.convolver.connect(context.destination);
-
-      } else if (this.convolver) {
-        this.volume.disconnect(0);
-        this.convolver.disconnect(0);
-        this.volume.connect(context.destination);
-      } else {
-        this.volume.connect(context.destination);
-      }
-
-      console.log( '** PLAY **' );
     }
 
-    this.print = function () {
-      console.log( '** PRINT ** ' + sample_url );
-    }
   }
 
-  function ReverbObject( reverb_url ) {
-    var reverb_url = reverb_url;
-    this.source = reverb_url;
-    loadAudio(this, reverb_url);
-  }
+  //var convolver;
+  //convolver = context.createConvolver();
+  //convolver.connect(context.destination);
 
-  irHall = new ReverbObject( '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409273irHall.ogg' );
+  var c = context.createConvolver();
+  c.connect(context.destination);
+  loadAudio(c, '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409273irHall.ogg');
+
+
   a = new SampleSound( '//dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409276tin.wav' );
-
+  a.volume.connect(c);
 
   $( document ).keydown(function( event ) {
     a.play();
