9e30744a0e8e2870c720eea909614fcaa83a45f3
petercorlettwiley <peter.c.wiley@gmail.com>
2015-12-23 21:15:32 -0600

    Oscillator objects are more modular

--- public/js/test_tone.js
+++ public/js/test_tone.js
@@ -33,7 +33,7 @@ $( document ).ready(function() {
 
     this.gain = context.createGain();
 
-    this.play = function () {
+    this.trigger = function () {
       var s = context.createBufferSource();
       s.buffer = this.buffer;
       s.connect( this.gain );
@@ -59,44 +59,49 @@ $( document ).ready(function() {
   a.gain.connect( c );
   a.gain.connect( context.destination );
 
+  // OSC
+  // ===
 
-  oscillator = context.createOscillator(); // Create sound source
-  oscillator.type = 'sine';
-  oscillator.frequency.value = 300;
-
-  oscillator.attackTime = 0.01;
-  oscillator.releaseTime = 1;
-
-  oscillator.gain = context.createGain();
-  oscillator.gain.gain.value = 0;
-
-  oscillator.connect(oscillator.gain);
-  oscillator.gain.connect(c);
-  oscillator.gain.connect(context.destination);
-  oscillator.start(0);
-
-  oscillator.trigger = function () {
-    var now = context.currentTime;
-    var currentGain = this.gain.gain.value;
-
-    this.gain.gain.cancelScheduledValues(now);
-
-    this.gain.gain.setValueAtTime(currentGain, now);
-    this.gain.gain.linearRampToValueAtTime(1.0, now + this.attackTime);
-    this.gain.gain.linearRampToValueAtTime(0, now + this.attackTime + this.releaseTime );
+  function Oscillator( object, frequency, attackTime, releaseTime ) {
+    object.type = 'sine';
+    object.frequency.value = frequency;
+  
+    object.attackTime = attackTime;
+    object.releaseTime = releaseTime;
+  
+    object.gain = context.createGain();
+    object.gain.gain.value = 0;
+  
+    object.connect(object.gain);
+    object.gain.connect(c);
+    object.gain.connect(context.destination);
+    object.start(0);
+  
+    object.trigger = function () {
+      var now = context.currentTime;
+      var currentGain = this.gain.gain.value;
+  
+      this.gain.gain.cancelScheduledValues(now);
+      this.gain.gain.setValueAtTime(currentGain, now);
+      this.gain.gain.linearRampToValueAtTime(1.0, now + this.attackTime);
+      this.gain.gain.linearRampToValueAtTime(0, now + this.attackTime + this.releaseTime );
+    }
   }
 
-  oscillator.gain.connect(c);
-  oscillator.gain.connect(context.destination);
+  o = context.createOscillator();
+  Oscillator( o, 300, 0.125, 0.3 );
+
+  o.gain.connect(c);
+  o.gain.connect(context.destination);
 
 
   // for testing sound triggering
   $( document ).keydown( function( event ) {
     if (event.keyCode == 32) { // space
-      a.play();
+      a.trigger();
     }
     if (event.keyCode == 16) { // shift
-      oscillator.trigger();
+      o.trigger();
     }
   });
 
