d16a48e6f62c85741a52a3427b0a620e4026d56d
Peter Wiley <peter.c.wiley@gmail.com>
2016-05-17 18:11:47 -0500

    Super basic data model is alive

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
-
+require 'curb'
+require 'json'
 
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, "#{ENV['DATABASE_URL']}")
@@ -10,6 +11,7 @@ class Post
   property :id, Serial
   property :title, String
   property :body, Text
+  property :published, Boolean, :default => false
 end
 
 DataMapper.finalize
@@ -17,23 +19,86 @@ DataMapper.auto_upgrade!
 
 class PCWILEY < Sinatra::Base
 
-  get '/' do
+  # authorize user
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? &&
+    @auth.basic? &&
+    @auth.credentials &&
+    @auth.credentials == [ENV['PC_USERNAME'],ENV['PC_PASSWORD']]
+  end
+
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="what's up")
+      throw(:halt, [401, "hi haters"])
+    end
+  end
+
+  get '/design' do
      # get the latest 20 posts #@posts = Post.all(:order => [ :id.desc ], :limit => 20)
     @posts = Post.all
-    erb :index
+    erb :posts
   end
 
-  get '/new' do
-    posty = Post.create(:title => 'posty 2!', :body => 'booody booody')
-    puts '#{posty.title}'
+  get '/admin' do
+    protected!
+    erb :admin
   end
 
-  get '/reset' do
+  get '/admin/newpost' do
+    protected!
+    erb :post_new
+  end
+
+  post '/admin/newpost' do
+    newPost = Post.create(:title => params[:title], :body => params[:body], :published => params[:published])
+    redirect '/admin'
+  end
+
+  get '/admin/reset' do
     Post.destroy
+    redirect '/admin'
   end
 
-  get '/tone' do
+  get '/test/tone' do
+    protected!
     erb :test_tone
+  end
+
+  get '/test/sentiment' do
+
+    c = Curl::Easy.new
+    url = "http://www.sentiment140.com/api/bulkClassifyJson?appid=peter.c.wiley@gmail.com"
+    headers = {}
+    headers['Content-Type'] = 'application/json'
+    headers['X-Requested-With'] = 'XMLHttpRequest'
+    headers['Accept'] = 'application/json'
+    payload = "{'data': [{'text': 'I love Titanic.'}]}"
+    
+    c.url = url
+    c.headers = headers
+    c.verbose = true
+
+    c.http_post(payload)
+
+    hash = JSON.parse c.body_str
+    puts hash
+    puts hash['data']
+    puts hash['data'][0]['text']
+
+    c.on_success {|easy| puts "success, add more easy handles" }
+
+  end
+
+  get '/:id' do
+    @page = Post.get(params[:id])
+
+    unless @page.published
+      halt 404, '<h1>Not Found</h1>'
+    end
+
+    erb :page
   end
   
 end
